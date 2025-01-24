26b2227c9a059d37745b91c254c61554c978275c
Josias Moukpe <erud1t3.devs@gmail.com>
2025-01-24 15:52:50 -0500

    current progress

---
+++ agent/tools/egd/ann.py
@@ -0,0 +1,371 @@
+
+import math
+import random
+
+class ANN:
+    '''
+    Feed Forward Artificial Neural Network Class
+    1 Input, 1 Hidden, 1 Output Layer
+    '''
+    def __init__(
+        self, 
+        net_id,
+        hyperparams,
+        input_units, 
+        output_units, 
+        debug=True,
+    ):
+        
+        '''
+        Initialize the Artificial Neural Network
+        '''
+        # hyperparameters
+        self.net_id = net_id
+        self.hidden_units = hyperparams['hidden_units']
+        self.learning_rate = hyperparams['learning_rate']
+        self.momentum = hyperparams['momentum']
+        self.decay = hyperparams['decay']
+        self.input_units = input_units
+        self.output_units = output_units
+        self.debug = debug
+
+        # initialize the weights at random based 
+        # the topology of the network
+        self.topology = [self.input_units] + \
+                        hyperparams['hidden_units'] + \
+                        [self.output_units]
+
+        self.weights = {
+            f'W{i}{i-1}': [[self.rand_init() 
+                        for _ in range(self.topology[i-1] + 1)]
+                    for _ in range(self.topology[i])] 
+                for i in range(1, len(self.topology))
+        }
+
+        self.res = None
+        # self.num_params = self.num_params()
+
+        # print the everything
+        # if self.debug:
+        #     print('learning rate: ', self.learning_rate)
+        #     print('momentum: ', self.momentum)
+        #     print('Weights: ', self.weights)
+        #     print('Topology: ', self.topology)
+
+    def rand_init(self):
+        '''
+        Initialize the weights at random
+        https://machinelearningmastery.com/weight-initialization-for-deep-learning-neural-networks/
+        '''
+        # number of nodes in the previous layer
+        n = self.input_units
+        # calculate the range for the weights
+        lower, upper = -(1.0 / math.sqrt(n)), (1.0 / math.sqrt(n))
+        # generate random numbers
+        number = random.random()
+        # scale to the desired range
+        scaled = lower + number * (upper - lower)
+
+        return scaled
+             
+    def print_weights(self):
+        '''
+        Print the weights of the Artificial Neural Network
+        with 2 decimal places
+        '''
+        # print('Weights: ', self.weights)
+        for key, value in self.weights.items():
+            print(f'{key}: ')
+            for i in range(len(value)):
+                print(f'{i}: ', end='')
+                for j in range(len(value[i])):
+                    print(f'{value[i][j]:.2f} ', end='')
+                print()
+
+    def print_network(self):
+        '''
+        Print the network
+        '''
+        print('Network: ', self.topology)
+        self.print_weights()
+
+    def set_hyperparameters(self, hyperparams):
+        '''
+        Set the hyperparameters of the Artificial Neural Network
+        '''
+        self.learning_rate = hyperparams['learning_rate']
+        self.momentum = hyperparams['momentum']
+        self.decay = hyperparams['decay']
+        self.hidden_units = hyperparams['hidden_units']
+        self.topology = [self.input_units] + \
+            hyperparams['hidden_units'] + \
+            [self.output_units]
+    
+    def num_params(self):
+        '''
+        Get the number of parameters
+        '''
+        num_parameters = 0
+        for t in range(1, len(self.topology)):
+            num_parameters += self.topology[t] * self.topology[t-1]
+        return num_parameters
+
+    def save(self, filename=None):
+        '''
+        Save the Artificial Neural Network
+        '''
+        # if no filename is provided, then use the default
+        filename = filename or 'weights.txt'
+        # save the weights onto a file
+        with open(filename, 'w') as f:
+            f.write(str(self.weights))
+
+    def load(self, filename):
+        '''
+        Load the Artificial Neural Network
+        '''
+        # load the weights from a file
+        with open(filename, 'r') as f:
+            self.weights = eval(f.read())
+
+        # print('one weight: ', self.weights['hidden'][0][0])
+        self.print_weights()
+
+    def get_classes(self):
+        '''
+        Get the output classes
+        '''
+        
+        num_out = self.output_units
+        classes = [
+            [0.0 for _ in range(num_out)] 
+            for _ in range(num_out)
+        ]
+
+        for i in range(num_out):
+            classes[i][i] = 1.0
+        
+        return classes
+
+    def sigmoid(self, x):
+        '''
+        Sigmoid activation function
+        '''
+        # print('bad x:', x)
+        try : return 1 / (1 + math.exp(-x))
+        except OverflowError: return 0.0
+
+    def d_sigmoid(self, x):
+        '''
+        Derivative of the sigmoid function
+        '''
+        y = self.sigmoid(x)
+        return y * (1 - y)
+
+    def forward(self, instance):
+        '''
+        Feed forward the Artificial Neural Network
+        '''
+
+        res = {
+            f'layer{i}': [0.0 for _ in range(self.topology[i])]
+                for i in range(1, len(self.topology))
+        }
+
+        # set the input layer to the instance
+        res['layer0'] = instance
+
+        # feed forward the hidden layer
+        for t in range(1, len(self.topology)):
+            for i in range(self.topology[t]):
+                for j in range(self.topology[t-1]):
+                    # calculating ther linear combination
+                    res[f'layer{t}'][i] += \
+                        self.weights[f'W{t}{t-1}'][i][j] * res[f'layer{t-1}'][j]
+                # adding the bias
+                res[f'layer{t}'][i] += \
+                    self.weights[f'W{t}{t-1}'][i][self.topology[t-1]]
+
+                # applying the activation function
+                res[f'layer{t}'][i] = self.sigmoid(res[f'layer{t}'][i])
+
+        # getting the output
+        output = res[f'layer{len(self.topology)-1}']
+        self.res = res
+
+        return output
+
+    def predict(self, instance):
+        '''
+        Predict the output of the instance
+        later update to include processing of discrete input 
+        and output
+        '''
+        return self.forward(instance)
+
+    def loss(self, target, output, no_decay=False):
+        '''
+        Compute the loss for SGD
+        '''
+        loss = 0.0
+        # getting all the loss
+        for i in range(self.output_units):
+            loss += (target[i] - output[i]) ** 2
+        loss /= 2.0
+
+        if no_decay: return loss 
+
+        w_term = 0.0
+        # adding all the weights
+        for l in range(1, len(self.topology)):
+            for i in range(len(self.weights[f'W{l}{l-1}'])):
+                for j in range(len(self.weights[f'W{l}{l-1}'][i])):
+                    w_term += self.weights[f'W{l}{l-1}'][i][j] ** 2
+
+        n = self.num_params()
+        loss += self.decay * (w_term / (n * 2))
+
+        return loss
+
+    def backward(self, target, output):
+        '''
+        Back propagate the error with momentum, weight 
+        decay and learning rate
+        SGD
+        '''
+        # prior delta update
+        errors = {
+            f'layer{l}': [0.0 for _ in range(self.topology[l])]
+                for l in range(1, len(self.topology))
+        }
+
+        # calculate the errors
+        for l in range(len(self.topology)-1, 0, -1):
+            for i in range(self.topology[l]):
+                if l == len(self.topology)-1:
+                    errors[f'layer{l}'][i] = target[i] - output[i]
+                else:
+                    for j in range(self.topology[l+1]):
+                        errors[f'layer{l}'][i] += errors[f'layer{l+1}'][j] * \
+                            self.weights[f'W{l+1}{l}'][j][i]
+                # applying the activation function derivative
+                errors[f'layer{l}'][i] *= self.d_sigmoid(self.res[f'layer{l}'][i])
+
+        return errors
+
+    def step(self, errors):
+        '''
+        Update the weights with the errors
+        '''
+        deltas = {
+            f'W{i}{i-1}': [[ 0.0 for _ in range(self.topology[i-1] + 1)]
+                    for _ in range(self.topology[i])] 
+                for i in range(1, len(self.topology))
+        }
+        # update the weights
+        for t in range(1, len(self.topology)):
+            for i in range(self.topology[t]):
+                for j in range(self.topology[t-1]):
+                    # update the weights
+                    deltas[f'W{t}{t-1}'][i][j] = self.learning_rate * errors[f'layer{t}'][i] * \
+                        self.res[f'layer{t-1}'][j] + self.momentum * deltas[f'W{t}{t-1}'][i][j]
+                    self.weights[f'W{t}{t-1}'][i][j] = (1 - self.learning_rate * self.decay) * \
+                        self.weights[f'W{t}{t-1}'][i][j] + deltas[f'W{t}{t-1}'][i][j]
+                
+                # update the bias
+                deltas[f'W{t}{t-1}'][i][self.topology[t-1]] = self.learning_rate * errors[f'layer{t}'][i] \
+                                    + self.momentum * deltas[f'W{t}{t-1}'][i][self.topology[t-1]]
+                self.weights[f'W{t}{t-1}'][i][self.topology[t-1]] = (1 - self.learning_rate * self.decay) * \
+                    self.weights[f'W{t}{t-1}'][i][self.topology[t-1]] + deltas[f'W{t}{t-1}'][i][self.topology[t-1]]
+
+    def training_step(self, train_data):
+        '''
+        Train the Artificial Neural Network
+        k is the number of folds
+        '''
+        if not train_data:
+            raise ValueError('No training data provided')
+        # get the data
+        data = train_data
+        # shuffle the data
+        random.shuffle(data)
+        # train the network
+        loss = 0.0
+        # if self.debug:
+        #     print('Epoch: ', i, end='')
+        for example in data:
+            # loss += self.step(instance)
+            inputt, target = example[0], example[1]
+            # get the output
+            output = self.forward(inputt)
+            # compute the loss
+            loss += self.loss(target, output)
+            # backpropagate the errors
+            errors = self.backward(target, output)
+            # update the weights
+            self.step(errors)
+            
+        # if self.debug:
+        #     # print('Weights: ', self.weights)
+        #     print(f'Net #{self.net_id}\'s loss: {loss/len(data): .3f}', end='')
+
+        return loss/len(data)
+
+    def train(self, train_data, epochs=10):
+        '''
+        Train the Artificial Neural Network
+        k is the number of folds
+        '''
+        if not train_data:
+            raise ValueError('No training data provided')
+        # get the data
+        data = train_data
+        # shuffle the data
+        random.shuffle(data)
+        # train the network
+        for _ in range(epochs):
+            # if self.debug:
+            #     print('Epoch: ', i, end='')
+            loss = 0.0
+            for example in data:
+                # loss += self.step(instance)
+                inputt, target = example[0], example[1]
+                # get the output
+                output = self.forward(inputt)
+                # compute the loss
+                loss += self.loss(target, output)
+                # backpropagate the errors
+                errors = self.backward(target, output)
+                # update the weights
+                self.step(errors)
+            
+            if self.debug:
+                # print('Weights: ', self.weights)
+                print(f'Net #{self.net_id}\'s loss: {loss/len(data)}', end='\n')
+
+        return loss/len(data)
+
+    def test(self, test_data=None):
+        '''
+        Test the Artificial Neural Network
+        ''' 
+        if not test_data:
+            raise Exception('No test data provided')
+        # if self.debug:
+        #     print('Testing data: ', test_data)
+        accuracy = 0.0
+        # test the network
+        for instance in test_data:
+            # get the output
+            output = self.forward(instance[0])
+            # check if the output is correct
+            accuracy += (1.0 - self.loss(instance[1], output, no_decay=True))
+        # get the average accuracy
+        accuracy /= len(test_data)
+
+        return accuracy
+ 
+    
+
+        
+
---
+++ agent/tools/egd/testMnist.py
@@ -0,0 +1,53 @@
+# imports
+from apbt import APBT
+
+
+def main():
+    '''main of the program'''
+
+    debugging = True
+    k = 80 # population size, 80 good number
+    epochs = 1000 # number of epochs
+
+
+    # run trials
+    for trial in range(3):
+        apbt = APBT(
+            k,
+            epochs,
+            debugging
+        )
+
+        # set log path
+        apbt.log_path= f'logs/mnist{trial}.csv'
+    
+        print('Trial: ', trial)
+        print('\nRunning the population based training\n')
+        best_net, most_acc = apbt.train()
+        print('\nPopulation Based Training complete\n')
+        # create the artificial neural network
+
+        # printing the neural network
+        print('\nPrinting learned weights of best\n')
+        best_net.print_network()
+        # test the artificial neural network
+        print('\nTesting the NN...\n')
+        accuracy = 100 * best_net.test(apbt.testing)
+        n_params = best_net.num_params()
+        print('\nTesting complete\n')
+        print(f'\nAccuracy: {accuracy:.2f}%\n')
+        print(f'Number of parameters: {n_params}\n')
+
+        # printing the neural network
+        print('\nPrinting learned weights of the most accurate\n')
+        most_acc.print_network()
+        # test the artificial neural network
+        print('\nTesting the NN...\n')
+        accuracy = 100 * most_acc.test(apbt.testing)
+        n_params = most_acc.num_params()
+        print('\nTesting complete\n')
+        print(f'\nAccuracy: {accuracy:.2f}%\n')
+        print(f'Number of parameters: {n_params}\n')
+    
+if __name__ == '__main__':
+    main()
